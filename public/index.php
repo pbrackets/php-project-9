d10cc81c9e2b156f04a99f1f110e7012beecb399
aelita <ai2work@yandex.ru>
2023-09-05 14:58:11 +0500

    зависимости

---
+++ public/index.php
@@ -0,0 +1,17 @@
+<?php declare(strict_types=1);
+namespace App;
+
+require '/composer/vendor/autoload.php';
+
+use Slim\Factory\AppFactory;
+
+// BEGIN
+$app = AppFactory::create();
+$app->addErrorMiddleware(true, true, true);
+
+$app->get('/', function ($request, $response) {
+    return $response->write('Welcome to Hexlet!');
+});
+
+$app->run();
+// END
