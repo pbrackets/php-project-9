b54ca7a2b0d403ef14b94818c2b11a7ef0adf5ce
aelita <ai2work@yandex.ru>
2023-09-08 11:49:22 +0500

    Hello

--- public/index.php
+++ public/index.php
@@ -2,23 +2,4 @@
 
 declare(strict_types=1);
 
-namespace App;
-
-require '/composer/vendor/autoload.php';
-
-use Slim\Factory\AppFactory;
-
-// BEGIN
-$app = AppFactory::create();
-$app->addErrorMiddleware(
-    true,
-    true,
-    true
-);
-
-$app->get('/', function ($request, $response) {
-    return $response->write('Welcome to Hexlet!');
-});
-
-$app->run();
-// END
+echo "Hello";
